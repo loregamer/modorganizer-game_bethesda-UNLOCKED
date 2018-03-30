80d2f8014798c8f6c0aa8e8237e66048b7091025
Silarn <jrim@rimpo.org>
2018-03-29 21:42:53 -0500

    [game_morrowind] Improved save parsing (and comments) and custom save info widget

--- src/games/morrowind/src/game_morrowind_en.ts
+++ src/games/morrowind/src/game_morrowind_en.ts
@@ -5,20 +5,70 @@
     <name>GameMorrowind</name>
     <message>
         <location filename="gamemorrowind.cpp" line="98"/>
-        <source>Adds support for the game Morrowind</source>
+        <source>Adds support for the game Morrowind.
+Splash by %1</source>
         <oldsource>Adds support for the game Morrowind</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MorrowindSaveGameInfoWidget</name>
+    <message>
+        <location filename="morrowindsavegameinfowidget.ui" line="39"/>
+        <source>Save #</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="morrowindsavegameinfowidget.ui" line="51"/>
+        <source>Character</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="morrowindsavegameinfowidget.ui" line="63"/>
+        <source>Level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="morrowindsavegameinfowidget.ui" line="75"/>
+        <source>Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="morrowindsavegameinfowidget.ui" line="87"/>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="morrowindsavegameinfowidget.cpp" line="73"/>
+        <source>Has Script Extender Data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="morrowindsavegameinfowidget.cpp" line="78"/>
+        <source>Missing ESPs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="morrowindsavegameinfowidget.cpp" line="111"/>
+        <location filename="morrowindsavegameinfowidget.cpp" line="149"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="morrowindsavegameinfowidget.cpp" line="117"/>
+        <source>Missing ESLs</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="morrowindgameplugins.cpp" line="92"/>
+        <location filename="morrowindgameplugins.cpp" line="90"/>
         <source>failed to set game file key (errorcode %1)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="morrowindgameplugins.cpp" line="100"/>
+        <location filename="morrowindgameplugins.cpp" line="98"/>
         <source>Some of your plugins have invalid names! These plugins can not be loaded by the game. Please see mo_interface.log for a list of affected plugins and rename them.</source>
         <translation type="unfinished"></translation>
     </message>
