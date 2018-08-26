5034ec6d090c8fe371af485ec3eb71226c69f95b
LostDragonist <lost.dragonist@gmail.com>
2018-08-26 10:07:52 -0500

    [game_morrowind] Change version to 0.2.1

--- src/games/morrowind/src/gamemorrowind.cpp
+++ src/games/morrowind/src/gamemorrowind.cpp
@@ -100,7 +100,7 @@ QString GameMorrowind::description() const
 
 MOBase::VersionInfo GameMorrowind::version() const
 {
-  return VersionInfo(0, 2, 0, VersionInfo::RELEASE_FINAL);
+  return VersionInfo(0, 2, 1, VersionInfo::RELEASE_FINAL);
 }
 
 bool GameMorrowind::isActive() const
