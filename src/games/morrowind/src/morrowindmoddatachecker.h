6fca8cc1a723c972475d9f3a02b20feaf11ae8bf
Mikaël Capelle <capelle.mikael@gmail.com>
2020-05-23 11:38:33 +0200

    [game_morrowind] Use a dedicated ModDataChecker.

---
+++ src/games/morrowind/src/morrowindmoddatachecker.h
@@ -0,0 +1,29 @@
+#ifndef MORROWIND_MODATACHECKER_H
+#define MORROWIND_MODATACHECKER_H
+
+#include <gamebryomoddatachecker.h>
+
+class MorrowindModDataChecker : public GamebryoModDataChecker
+{
+public:
+  using GamebryoModDataChecker::GamebryoModDataChecker;
+
+protected:
+  virtual const FileNameSet& possibleFolderNames() const override {
+    static FileNameSet result{
+      "fonts", "meshes", "music", "shaders", "sound", "textures", "video",
+      "mwse", "distantland", "mits", "icons", "bookart", "splash"
+    };
+    return result;
+  }
+  virtual const FileNameSet& possibleFileExtensions() const override {
+    static FileNameSet result{
+      "esp", "esm", "bsa", ".modgroups"
+    };
+    return result;
+  }
+
+  const GameGamebryo* m_Game;
+};
+
+#endif // MORROWIND_MODATACHECKER_H
