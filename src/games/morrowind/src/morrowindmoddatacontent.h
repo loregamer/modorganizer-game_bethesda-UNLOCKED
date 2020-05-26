4cec11b6c74ec22c414942d7ae10b0882bc94148
Mikaël Capelle <capelle.mikael@gmail.com>
2020-05-26 21:27:46 +0200

    [game_morrowind] Custom ModDataContent for Morrowind.

---
+++ src/games/morrowind/src/morrowindmoddatacontent.h
@@ -0,0 +1,23 @@
+#ifndef MORROWIND_MODDATACONTENT_H
+#define MORROWIND_MODDATACONTENT_H
+
+#include <gamebryomoddatacontent.h>
+#include <ifiletree.h>
+
+class MorrowindModDataContent : public GamebryoModDataContent {
+public:
+
+  /**
+   *
+   */
+  MorrowindModDataContent(GameGamebryo const* gamePlugin) : GamebryoModDataContent(gamePlugin) {
+    // Just need to disable some contents:
+    m_Enabled[CONTENT_MCM] = false;
+    m_Enabled[CONTENT_SKYPROC] = false;
+    m_Enabled[CONTENT_INTERFACE] = false;
+    m_Enabled[CONTENT_SCRIPT] = false;
+  }
+
+};
+
+#endif // MORROWIND_MODDATACONTENT_H
