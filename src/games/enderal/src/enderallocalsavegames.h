6eb86ad28e77081da9abf6e6a017551c970590c1
Mikaël Capelle <capelle.mikael@gmail.com>
2024-06-09 12:32:11 +0200

    [game_enderal] Refactoring following uibase change for game features. (#27)

--- src/games/enderal/src/enderallocalsavegames.h
+++ src/games/enderal/src/enderallocalsavegames.h
@@ -24,7 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <QDir>
 #include <QString>
 
-class EnderalLocalSavegames : public LocalSavegames
+class EnderalLocalSavegames : public MOBase::LocalSavegames
 {
 
 public:
