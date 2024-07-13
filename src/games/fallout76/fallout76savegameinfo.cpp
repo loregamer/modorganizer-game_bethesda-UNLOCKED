2a1fe115f62d215ee1fb70ca259c2e0babd68899
Mikaël Capelle <capelle.mikael@gmail.com>
2024-07-13 12:35:00 +0200

    Reorganize CMake stuff and update after changes to mo2-cmake.

---
+++ src/games/fallout76/fallout76savegameinfo.cpp
@@ -0,0 +1,10 @@
+#include "fallout76savegameinfo.h"
+
+#include "fallout76savegame.h"
+#include "gamegamebryo.h"
+
+Fallout76SaveGameInfo::Fallout76SaveGameInfo(GameGamebryo const* game)
+    : GamebryoSaveGameInfo(game)
+{}
+
+Fallout76SaveGameInfo::~Fallout76SaveGameInfo() {}
