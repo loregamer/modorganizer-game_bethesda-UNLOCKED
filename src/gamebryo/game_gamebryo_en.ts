8f2d47a9123e3ef890994eb5a439e8d602c4811b
Mikaël Capelle <capelle.mikael@gmail.com>
2024-07-13 12:35:00 +0200

    Reorganize CMake stuff and update after changes to mo2-cmake.

--- src/gamebryo/game_gamebryo_en.ts
+++ src/gamebryo/game_gamebryo_en.ts
@@ -138,6 +138,7 @@
     <name>QObject</name>
     <message>
         <location filename="gamebryogameplugins.cpp" line="130"/>
+        <location filename="../creation/creationgameplugins.cpp" line="97"/>
         <source>Some of your plugins have invalid names! These plugins can not be loaded by the game. Please see mo_interface.log for a list of affected plugins and rename them.</source>
         <translation type="unfinished"></translation>
     </message>
