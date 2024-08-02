f22378ca70bf8026390c478b7f0855437d62ca44
Jeremy Rimpo <jrim@rimpo.org>
2024-08-01 21:08:42 -0500

    [game_fallout4london] Initial migration from original FO4 plugin

---
+++ src/games/fallout4london/src/gamefo4london.cpp
@@ -0,0 +1,335 @@
+#include "gameFallout4London.h"
+
+#include "fallout4bsainvalidation.h"
+#include "fallout4dataarchives.h"
+#include "fallout4moddatachecker.h"
+#include "fallout4moddatacontent.h"
+#include "fallout4savegame.h"
+#include "fallout4scriptextender.h"
+#include "fallout4unmanagedmods.h"
+
+#include "versioninfo.h"
+#include <creationgameplugins.h>
+#include <executableinfo.h>
+#include <gamebryolocalsavegames.h>
+#include <gamebryosavegameinfo.h>
+#include <pluginsetting.h>
+
+#include <QCoreApplication>
+#include <QDir>
+#include <QFileInfo>
+#include <QList>
+#include <QObject>
+#include <QString>
+#include <QStringList>
+
+#include <memory>
+
+#include "scopeguard.h"
+
+using namespace MOBase;
+
+GameFallout4London::GameFallout4London() {}
+
+bool GameFallout4London::init(IOrganizer* moInfo)
+{
+  if (!GameGamebryo::init(moInfo)) {
+    return false;
+  }
+
+  auto dataArchives = std::make_shared<Fallout4LondonDataArchives>(this);
+
+  registerFeature(std::make_shared<Fallout4LondonScriptExtender>(this));
+  registerFeature(dataArchives);
+  registerFeature(std::make_shared<GamebryoLocalSavegames>(this, "fallout4custom.ini"));
+  registerFeature(std::make_shared<Fallout4LondonModDataChecker>(this));
+  registerFeature(
+      std::make_shared<Fallout4LondonModDataContent>(m_Organizer->gameFeatures()));
+  registerFeature(std::make_shared<GamebryoSaveGameInfo>(this));
+  registerFeature(std::make_shared<CreationGamePlugins>(moInfo));
+  registerFeature(std::make_shared<Fallout4LondonUnmangedMods>(this));
+  registerFeature(std::make_shared<Fallout4LondonBSAInvalidation>(dataArchives.get(), this));
+
+  return true;
+}
+
+QString GameFallout4London::gameName() const
+{
+  return "Fallout 4";
+}
+
+void GameFallout4London::detectGame()
+{
+  m_GamePath    = identifyGamePath();
+  m_MyGamesPath = determineMyGamesPath("Fallout4London");
+}
+
+QList<ExecutableInfo> GameFallout4London::executables() const
+{
+  return QList<ExecutableInfo>()
+         << ExecutableInfo("F4SE",
+                           findInGameFolder(m_Organizer->gameFeatures()
+                                                ->gameFeature<MOBase::ScriptExtender>()
+                                                ->loaderName()))
+         << ExecutableInfo("Fallout 4", findInGameFolder(binaryName()))
+         << ExecutableInfo("Fallout Launcher", findInGameFolder(getLauncherName()))
+         << ExecutableInfo("Creation Kit", findInGameFolder("CreationKit.exe"))
+                .withSteamAppId("1946160")
+         << ExecutableInfo("LOOT", QFileInfo(getLootPath()))
+                .withArgument("--game=\"Fallout4\"");
+}
+
+QList<ExecutableForcedLoadSetting> GameFallout4London::executableForcedLoads() const
+{
+  return QList<ExecutableForcedLoadSetting>();
+}
+
+QString GameFallout4London::name() const
+{
+  return "Fallout 4 Support Plugin";
+}
+
+QString GameFallout4London::localizedName() const
+{
+  return tr("Fallout 4 Support Plugin");
+}
+
+QString GameFallout4London::author() const
+{
+  return "Tannin & MO2 Team";
+}
+
+QString GameFallout4London::description() const
+{
+  return tr("Adds support for the game Fallout 4.\n"
+            "Splash by %1")
+      .arg("nekoyoubi");
+}
+
+MOBase::VersionInfo GameFallout4London::version() const
+{
+  return VersionInfo(1, 8, 0, VersionInfo::RELEASE_FINAL);
+}
+
+QList<PluginSetting> GameFallout4London::settings() const
+{
+  return QList<PluginSetting>();
+}
+
+MappingType GameFallout4London::mappings() const
+{
+  MappingType result;
+  if (testFilePlugins().isEmpty()) {
+    for (const QString& profileFile : {"plugins.txt", "loadorder.txt"}) {
+      result.push_back({m_Organizer->profilePath() + "/" + profileFile,
+                        localAppFolder() + "/" + gameShortName() + "/" + profileFile,
+                        false});
+    }
+  }
+  return result;
+}
+
+void GameFallout4London::initializeProfile(const QDir& path, ProfileSettings settings) const
+{
+  if (settings.testFlag(IPluginGame::MODS)) {
+    copyToProfile(localAppFolder() + "/Fallout4", path, "plugins.txt");
+  }
+
+  if (settings.testFlag(IPluginGame::CONFIGURATION)) {
+    if (settings.testFlag(IPluginGame::PREFER_DEFAULTS) ||
+        !QFileInfo(myGamesPath() + "/fallout4.ini").exists()) {
+      copyToProfile(gameDirectory().absolutePath(), path, "fallout4_default.ini",
+                    "fallout4.ini");
+    } else {
+      copyToProfile(myGamesPath(), path, "fallout4.ini");
+    }
+
+    copyToProfile(myGamesPath(), path, "fallout4prefs.ini");
+    copyToProfile(myGamesPath(), path, "fallout4custom.ini");
+  }
+}
+
+QString GameFallout4London::savegameExtension() const
+{
+  return "fos";
+}
+
+QString GameFallout4London::savegameSEExtension() const
+{
+  return "f4se";
+}
+
+std::shared_ptr<const GamebryoSaveGame>
+GameFallout4London::makeSaveGame(QString filePath) const
+{
+  return std::make_shared<const Fallout4LondonSaveGame>(filePath, this);
+}
+
+QString GameFallout4London::steamAPPId() const
+{
+  return "377160";
+}
+
+QStringList GameFallout4::testFilePlugins() const
+{
+  QStringList plugins;
+  if (m_Organizer != nullptr && m_Organizer->profile() != nullptr) {
+    QString customIni(
+        m_Organizer->profile()->absoluteIniFilePath("Fallout4Custom.ini"));
+    if (QFile(customIni).exists()) {
+      for (int i = 1; i <= 10; ++i) {
+        QString setting("sTestFile");
+        setting += std::to_string(i);
+        WCHAR value[MAX_PATH];
+        DWORD length = ::GetPrivateProfileStringW(
+            L"General", setting.toStdWString().c_str(), L"", value, MAX_PATH,
+            customIni.toStdWString().c_str());
+        if (length && wcscmp(value, L"") != 0) {
+          QString plugin = QString::fromWCharArray(value, length);
+          if (!plugin.isEmpty() && !plugins.contains(plugin))
+            plugins.append(plugin);
+        }
+      }
+    }
+  }
+  return plugins;
+}
+
+QStringList GameFallout4::primaryPlugins() const
+{
+  QStringList plugins = {"fallout4.esm",      "dlcrobot.esm",
+                         "dlcworkshop01.esm", "dlccoast.esm",
+                         "dlcworkshop02.esm", "dlcworkshop03.esm",
+                         "dlcnukaworld.esm",  "dlcultrahighresolution.esm"};
+
+  auto testPlugins = testFilePlugins();
+  if (loadOrderMechanism() == LoadOrderMechanism::None) {
+    plugins << testPlugins;
+  } else {
+    plugins << CCPlugins();
+  }
+
+  return plugins;
+}
+
+QStringList GameFallout4::gameVariants() const
+{
+  return {"Regular"};
+}
+
+QString GameFallout4::gameShortName() const
+{
+  return "Fallout4";
+}
+
+QString GameFallout4::gameNexusName() const
+{
+  return "fallout4";
+}
+
+QStringList GameFallout4::iniFiles() const
+{
+  return {"fallout4.ini", "fallout4prefs.ini", "fallout4custom.ini"};
+}
+
+QStringList GameFallout4::DLCPlugins() const
+{
+  return {"dlcrobot.esm",
+          "dlcworkshop01.esm",
+          "dlccoast.esm",
+          "dlcworkshop02.esm",
+          "dlcworkshop03.esm",
+          "dlcnukaworld.esm",
+          "dlcultrahighresolution.esm"};
+}
+
+QStringList GameFallout4::CCPlugins() const
+{
+  QStringList plugins = {};
+  QFile file(gameDirectory().absoluteFilePath("Fallout4.ccc"));
+  if (file.open(QIODevice::ReadOnly)) {
+    ON_BLOCK_EXIT([&file]() {
+      file.close();
+    });
+
+    if (file.size() == 0) {
+      return plugins;
+    }
+    while (!file.atEnd()) {
+      QByteArray line = file.readLine().trimmed();
+      QString modName;
+      if ((line.size() > 0) && (line.at(0) != '#')) {
+        modName = QString::fromUtf8(line.constData()).toLower();
+      }
+
+      if (modName.size() > 0) {
+        if (!plugins.contains(modName, Qt::CaseInsensitive)) {
+          plugins.append(modName);
+        }
+      }
+    }
+  }
+  return plugins;
+}
+
+IPluginGame::SortMechanism GameFallout4::sortMechanism() const
+{
+  if (!testFilePresent())
+    return IPluginGame::SortMechanism::LOOT;
+  return IPluginGame::SortMechanism::NONE;
+}
+
+IPluginGame::LoadOrderMechanism GameFallout4::loadOrderMechanism() const
+{
+  if (!testFilePresent())
+    return IPluginGame::LoadOrderMechanism::PluginsTxt;
+  return IPluginGame::LoadOrderMechanism::None;
+}
+
+int GameFallout4::nexusModOrganizerID() const
+{
+  return 28715;
+}
+
+int GameFallout4::nexusGameID() const
+{
+  return 1151;
+}
+
+// Start Diagnose
+std::vector<unsigned int> GameFallout4::activeProblems() const
+{
+  std::vector<unsigned int> result;
+  if (m_Organizer->managedGame() == this) {
+    if (testFilePresent())
+      result.push_back(PROBLEM_TEST_FILE);
+  }
+  return result;
+}
+
+bool GameFallout4::testFilePresent() const
+{
+  if (!testFilePlugins().isEmpty())
+    return true;
+  return false;
+}
+
+QString GameFallout4::shortDescription(unsigned int key) const
+{
+  switch (key) {
+  case PROBLEM_TEST_FILE:
+    return tr("sTestFile entries are present");
+  }
+}
+
+QString GameFallout4::fullDescription(unsigned int key) const
+{
+  switch (key) {
+  case PROBLEM_TEST_FILE: {
+    return tr("<p>You have sTestFile settings in your "
+              "Fallout4Custom.ini. These must be removed or "
+              "the game will not read the plugins.txt file. "
+              "Management is disabled.</p>");
+  }
+  }
+}
